5c48450210d8dcdfa6e62d156ac9be1480825fc1
ota-renato <ota.renato@gmail.com>
2021-11-05 18:05:28 -0300

    Aplicando comentarios de funcionamento

--- src/app/shared/services/host.service.ts
+++ src/app/shared/services/host.service.ts
@@ -7,6 +7,13 @@ export class HostService {
 
   constructor() { }
 
+  /**
+   * Método para capturar através da url, onde o projeto está em funcionamento
+   * e passar o caminho correto
+   *
+   * @returns o caminho (root) do path
+   *
+   */
   getDomainUrl(): string {
     const host = window.location.hostname;
     const url = host === 'localhost' ? '../../../' : 'https://otarenato.github.io/fgv-ordenacao-livros/';
--- src/app/shared/services/livros.service.ts
+++ src/app/shared/services/livros.service.ts
@@ -11,6 +11,13 @@ export class LivrosService {
 
   constructor(private http: HttpClient, private hostService: HostService) { }
 
+  /**
+   * Método para buscar no mock, os livros cadastrados e trazer em forma de um array de objetos
+   * para a aplicação consumir
+   *
+   * @returns um array de objetos do tipo "Livro"
+   *
+   */
   listarLivros(): Observable<Livro[]> {
     const URL = this.hostService.getDomainUrl();
     return this.http.get<Livro[]>(`${ URL }assets/data/livros.json`);
