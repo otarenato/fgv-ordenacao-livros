d7510bc7e012d30c69dfe9ab3d65a798dff5ddbb
ota-renato <ota.renato@gmail.com>
2021-11-05 16:53:53 -0300

    commit

--- src/app/shared/services/livros.service.ts
+++ src/app/shared/services/livros.service.ts
@@ -2,15 +2,19 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Livro } from '../models';
+import { HostService } from './host.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LivrosService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private hostService: HostService) { }
+
+
 
   listarLivros(): Observable<Livro[]> {
-    return this.http.get<Livro[]>('../../../assets/data/livros.json');
+    const URL = this.hostService.getDomainUrl();
+    return this.http.get<Livro[]>(`${ URL }assets/data/livros.json`);
   }
 }
